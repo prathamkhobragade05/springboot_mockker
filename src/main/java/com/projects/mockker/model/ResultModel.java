dbdad097726355c11654e63175bc82dcacaea0e9
PrAtHaM KhObRaGaDe <prathamkhobragade05@gmail.com>
2025-12-12 17:22:04 +0530

    Update ResultModel.java

--- src/main/java/com/projects/mockker/model/ResultModel.java
+++ src/main/java/com/projects/mockker/model/ResultModel.java
@@ -24,6 +24,9 @@ public class ResultModel {
     @Column(name="test_id")
     private Long testId;
     
+    @Column(name="topic_id")
+    private Long topicId;
+    
     @Column(name="score")
     private String score;
     @Column(name="date_time")
@@ -31,10 +34,11 @@ public class ResultModel {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime  dateTime;
 
-    public ResultModel(Long id,Long userId, Long testId, String score,LocalDateTime dateTime){
+    public ResultModel(Long id,Long userId, Long testId,Long topicId, String score,LocalDateTime dateTime){
         this.id=id;
         this.userId=userId;
         this.testId=testId;
+        this.topicId=topicId;
         this.score=score;
         this.dateTime=dateTime;
     }
@@ -43,12 +47,14 @@ public class ResultModel {
     public Long getId() {return id;}
     public Long getUserId() {return userId;}
     public Long getTestId() {return testId;}
+    public Long getTopicId() {return topicId;}
     public String getScore() {return score;}
     public LocalDateTime getDateTime() {return dateTime;}
 
     public void setId(Long id) {this.id = id;}
     public void setUserId(Long userId) {this.userId = userId;}
     public void setTestId(Long testId) {this.testId = testId;}
+    public void setTopicid(Long topicId) {this.topicId= topicId;}
     public void setScore(String score) {this.score = score;}
     public void setDateTime(LocalDateTime dateTime) {this.dateTime = dateTime;}
 
