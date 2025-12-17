68d8b7081935e343c00a63ac5f90d88580863e60
PrAtHaM KhObRaGaDe <prathamkhobragade05@gmail.com>
2025-12-17 15:07:44 +0530

    Update ResultModel.java

--- src/main/java/com/projects/mockker/model/ResultModel.java
+++ src/main/java/com/projects/mockker/model/ResultModel.java
@@ -14,7 +14,7 @@ import lombok.Data;
 @Table(name="Results")
 public class ResultModel {
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="result_id" )
     private Long id;
 
